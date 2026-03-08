5fd7f22a9704cb0771d28395758375f18af567dd
Rejoe <gnanav948@gmail.com>
2026-03-08 22:12:51 +0530

    initial commit

---
+++ src/component/landingpage.jsx
@@ -0,0 +1,16 @@
+import './landingpage.css'
+import { Search, Bell, User } from "lucide-react";
+
+function LandingPage() {
+  return (
+    <>
+      <div className="section1">
+          <div className="heading">
+            <h1>Mindlock AI</h1>
+          </div>
+      </div>
+    </>
+  )
+}
+
+export default LandingPage

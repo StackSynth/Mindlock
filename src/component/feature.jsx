f219896ff9d7c08e5b5413a1f67286f297ed6f35
Rejoe <gnanav948@gmail.com>
2026-03-09 22:17:53 +0530

    initial commit

--- src/component/feature.jsx
+++ src/component/feature.jsx
@@ -4,7 +4,6 @@ import { motion } from 'framer-motion';
 function Feature({ onNavigate }) {
     return (
         <>
-            {/* Neural Network Background */}
             <div className="neural-background">
                 <div className="network-container">
                     {[...Array(25)].map((_, i) => (
